bbff25e8632a35f2ceb8c96ce9b5afe7ba749c31
samdiobar <sbp5602@gmail.com>
2023-10-08 18:25:08 -0500

    completed 6.17

--- hw2/6.17/main.py
+++ hw2/6.17/main.py
@@ -1,11 +1,15 @@
 def passwordChecker(user_input):
   result = user_input
 
-
+  result = result.replace("i", "!")
+  result = result.replace("a", "@")
+  result = result.replace("m", "M")
+  result = result.replace("B", "8")
+  result = result.replace("o", ".")
+  result = result + "q*s"
 
   return result
 
 if __name__ == "__main__":
-  print("Please input your password: ",end="")
-  input_val = int(input())
+  input_val = str(input())
   print(passwordChecker(input_val))

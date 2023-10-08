f45bb75c90f8efaa8a69f3a924a7f8730a6bd4d4
samdiobar <sbp5602@gmail.com>
2023-10-08 18:17:16 -0500

    Started 6.17

---
+++ hw2/6.17/main.py
@@ -0,0 +1,11 @@
+def passwordChecker(user_input):
+  result = user_input
+
+
+
+  return result
+
+if __name__ == "__main__":
+  print("Please input your password: ",end="")
+  input_val = int(input())
+  print(passwordChecker(input_val))

b44193db7053067a85288c4eb6e5e04ee0d37504
samdiobar <sbp5602@gmail.com>
2023-10-08 21:06:11 -0500

    Completed 8.10 and 9.10

---
+++ hw2/8.10/main.py
@@ -0,0 +1,11 @@
+def palindrome(user_input):
+  text = user_input.replace(" ", "")
+
+  for i in range(0,int(len(text))):
+    if (text[i] != text[int(len(text)) - i - 1]):
+      return user_input + " is not a palindrome"
+  return user_input + " is a palindrome"
+
+if __name__ == "__main__":
+  user_input = str(input())
+  print(palindrome(user_input))
---
+++ hw2/9.10/main.py
@@ -0,0 +1,17 @@
+import csv
+
+def frequencies(input_file):
+  word_dict = {}
+
+  text = open(input_file, 'r').read().replace("\n", "").split(",")
+  for word in text:
+    if (word not in word_dict):
+      word_dict[word] = 1
+    else:
+      word_dict[word] += 1
+  for word in word_dict:
+    print(word, word_dict[word])
+
+if __name__ == "__main__":
+  user_input = str(input())
+  frequencies(user_input)

329f2f74c5f38fb9539abc2ebce64a2e5a1ab4ff
Sam Barroso <sbp5602@gmail.com>
2023-11-12 20:23:21 -0600

    completed parts b and c

--- FinalProjectPart1/FinalProjectMain.py
+++ FinalProjectPart1/FinalProjectMain.py
@@ -5,6 +5,40 @@ ID: 1844307
 
 import csv
 
+def dict_to_sorted_list(dictionary, sort_by):
+    new_list = []
+
+    for key in dictionary:
+        if sort_by == "ID":
+            new_list.append(key)
+        else:
+            new_list.append(dictionary[key][sort_by])
+
+    new_list.sort()
+    
+    for key in dictionary:
+        if sort_by == "ID":
+            index = new_list.index(key)
+        else:
+            index = new_list.index(dictionary[key][sort_by])
+        new_list[index] = dictionary[key]
+        new_list[index].update({"ID": key})
+    return new_list
+
+today = "11/12/2023"
+def time_greater_than(date1, date2):
+    date1_arr = date1.split("/")
+    date2_arr = date2.split("/")
+    if int(date1_arr[2]) > int(date2_arr[2]):
+        return True
+    elif int(date1_arr[2]) == int(date2_arr[2]):
+        if int(date1_arr[0]) > int(date2_arr[0]):
+            return True
+        elif int(date1_arr[0]) == int(date2_arr[0]):
+            if int(date1_arr[1]) > int(date2_arr[1]):
+                return True
+    return False
+
 class Students:
     def __init__(self, majors_list, gpa_list, graduation_dates_list):
         self.master_dict = {}
@@ -15,7 +49,7 @@ class Students:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
                 id = row[0]
-                self.master_dict[id] = {"LastName": row[1], "FirstName": row[2], "Major": row[3], "DisciplinaryAction": row[4]}
+                self.master_dict[id] = {"LastName": row[1], "FirstName": row[2], "Major": row[3], "DA": row[4]}
         with open(gpa_list) as csv_file:
             csv_reader = csv.reader(csv_file)
             for row in csv_reader:
@@ -27,25 +61,33 @@ class Students:
                 id = row[0]
                 self.master_dict[id].update({"GraduationDate": row[1]})
     
-    def print_dict(self):
-        for key in self.master_dict:
-            print(self.master_dict[key])
-
-    def get_full_roster(self):
-        last_names_list = []
-        for keys in self.master_dict:
-            last_names_list.append(self.master_dict[keys]["LastName"])
-
-        last_names_list.sort()
-
-        for key in self.master_dict:
-            index = last_names_list.index(self.master_dict[key]["LastName"])
-            last_names_list[index] = self.master_dict[key]
-            last_names_list[index].update({"ID": key})
-        return last_names_list
+    def create_by_majors_dict(self):
+        by_majors_dict = {}
+        for id in self.master_dict:
+            major = self.master_dict[id]["Major"]
+            try:
+                by_majors_dict[major][id] = self.master_dict[id]
+            except:
+                by_majors_dict[major] = {}
+                by_majors_dict[major][id] = self.master_dict[id]
+        return by_majors_dict
+    
+    def create_elegible_students_dict(self):
+        elegible_students_dict = {}
+        for id in self.master_dict:
+            gpa = float(self.master_dict[id]["GPA"])
+            if (gpa <= 3.8):
+                continue
+            if (self.master_dict[id]["DA"] == "Y"):
+                continue
+            if (time_greater_than(today, self.master_dict[id]["GraduationDate"])):
+                continue
+            elegible_students_dict[id] = self.master_dict[id]
+            elegible_students_dict[id]['GPA'] = gpa
+        return elegible_students_dict
 
 def write_list_to_file(file_name, list, order_of_params):
-    with open(file_name, 'w', encoding='UTF8', newline='') as f:
+    with open("FinalProject" + file_name + ".csv", 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
         for item in list:
             ordered_array = []
@@ -53,8 +95,25 @@ def write_list_to_file(file_name, list, order_of_params):
                 ordered_array.append(item[param])
             writer.writerow(ordered_array)
 
+def output_to_files(students, full_roster, elegible_students):
+    full_roster_list = dict_to_sorted_list(students.master_dict, "LastName")
+    order_of_params = ['ID', 'Major', 'FirstName', 'LastName', 'GPA', 'GraduationDate', 'DA']
+    write_list_to_file(full_roster, full_roster_list, order_of_params)
+
+    by_majors_dict = students.create_by_majors_dict()
+    order_of_params = ['ID', 'LastName', 'FirstName', 'GraduationDate', 'DA']
+    for major in by_majors_dict:
+        by_majors_list = dict_to_sorted_list(by_majors_dict[major], "ID")
+        write_list_to_file(major, by_majors_list, order_of_params)
+
+    elegible_students_dict = students.create_elegible_students_dict()
+    elegible_students_list = dict_to_sorted_list(elegible_students_dict, "GPA")
+    elegible_students_list.reverse()
+    order_of_params = ['ID', 'LastName', 'FirstName', 'Major', 'GPA']
+    write_list_to_file(elegible_students, elegible_students_list, order_of_params)
+    
+
+
 if __name__ == "__main__":
     student_manager = Students("./Inputs/StudentsMajorsList-3.csv", "./Inputs/GPAList-1.csv", "./Inputs/GraduationDatesList-1.csv")
-    full_roster = student_manager.get_full_roster()
-    order_of_params = ['ID', 'Major', 'FirstName', 'LastName', 'GPA', 'GraduationDate', 'DisciplinaryAction']
-    write_list_to_file("FullRoster.csv", full_roster, order_of_params)
+    output_to_files(student_manager, "FullRoster", "ScholarshipCandidates")

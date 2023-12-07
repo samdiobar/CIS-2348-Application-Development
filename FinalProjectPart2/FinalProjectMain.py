b2a4e74340f9d97fd9ebbe908df4681be9c4d21f
Sam Barroso <sbp5602@gmail.com>
2023-12-07 01:57:11 -0600

    completed final project part 2

---
+++ FinalProjectPart2/FinalProjectMain.py
@@ -0,0 +1,160 @@
+"""
+Name: Samuel Barroso
+ID: 1844307
+"""
+
+import csv
+
+def has_one_float(list):
+    cnt = 0
+    for str in list:
+        if str == list[-1] and list[-1][-1] == ".":
+            str = str[0:-1]
+        if (str.replace(".", "").isnumeric() and str.count(".") <= 1):
+            cnt += 1
+    if cnt == 1:
+        return True
+    return False
+
+def get_float(list):
+    for str in list:
+        if str == list[-1] and list[-1][-1] == ".":
+            str = str[0:-1]
+        if (str.replace(".", "").isnumeric() and str.count(".") <= 1):
+            return float(str)
+    return 0.0
+
+today = "12/7/2023"
+def time_greater_than(date1, date2):
+    date1_arr = date1.split("/")
+    date2_arr = date2.split("/")
+    if int(date1_arr[2]) > int(date2_arr[2]):
+        return True
+    elif int(date1_arr[2]) == int(date2_arr[2]):
+        if int(date1_arr[0]) > int(date2_arr[0]):
+            return True
+        elif int(date1_arr[0]) == int(date2_arr[0]):
+            if int(date1_arr[1]) > int(date2_arr[1]):
+                return True
+    return False
+
+def print_student(student):
+    print(f'{student["ID"]:10}{student["FirstName"]:14}{student["LastName"]:14}{student["GPA"]:7}')
+
+class Students:
+    # Class contructor
+    def __init__(self, majors_list, gpa_list, graduation_dates_list):
+        self.master_dict = {}
+        self.create_master_dict(majors_list, gpa_list, graduation_dates_list)
+    
+    # Reads information from the 3 input files and collects them all into one dictionary
+    def create_master_dict(self, majors_list, gpa_list, graduation_dates_list):
+        with open(majors_list) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                id = row[0]
+                self.master_dict[id] = {"LastName": row[1], "FirstName": row[2], "Major": row[3], "DA": row[4]}
+        with open(gpa_list) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                id = row[0]
+                self.master_dict[id].update({"GPA": row[1]})
+        with open(graduation_dates_list) as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for row in csv_reader:
+                id = row[0]
+                self.master_dict[id].update({"GraduationDate": row[1]})
+
+def print_menu(students):
+    student_dict = students.master_dict
+    user_input = input("Provide Student major and GPA: \n")
+    print()
+    user_input_lower = user_input.lower()
+    while user_input != "q":
+        # get major user_input
+        #   - Requires knowing the list of majors from Students class
+        # get GPA from user_input 
+        #   - find Students 0.1 of the requested GPA
+        #   - find Students 0.25 of the requested GPA 
+        #   - Do not provide students that have graduated or had disciplinary action
+        # if no students found,
+        #   provide the student within the requested major with closest GPA to that requested.
+        #   do not provide students that have graduated or had disciplinary action
+
+        majors_set = {student_dict[id]["Major"].lower() for id in student_dict}
+        if not any(user_input_lower.count(major) == 1 for major in majors_set):
+            print("No such student")
+        elif not has_one_float(user_input.split(" ")):
+            print("No such student")
+        else:
+            input_major = ""
+            for major in majors_set:
+                if major in user_input_lower:
+                    input_major = major
+            input_gpa = get_float(user_input.split(" "))
+
+            students_under_1 = []
+            students_under_25 = []
+            student_closest_gpa = None
+            for id in student_dict:
+                if student_dict[id]["Major"] != input_major.title():
+                    continue
+                if student_dict[id]["DA"] != "":
+                    continue
+                if not time_greater_than(today, student_dict[id]["GraduationDate"]):
+                    continue
+                
+                if -0.101 <= float(student_dict[id]["GPA"]) - input_gpa <= 0.101:
+                    temp = student_dict[id]
+                    temp["ID"] = id
+                    students_under_1.append(temp)
+                elif -0.251 <= float(student_dict[id]["GPA"]) - input_gpa <= 0.251:
+                    temp = student_dict[id]
+                    temp["ID"] = id
+                    students_under_25.append(temp)
+                else:
+                    if student_closest_gpa == None:
+                        temp = student_dict[id]
+                        temp["ID"] = id
+                        student_closest_gpa = temp
+                    elif abs(float(student_closest_gpa["GPA"]) - input_gpa) > abs(float(student_dict[id]["GPA"]) - input_gpa):
+                        temp = student_dict[id]
+                        temp["ID"] = id
+                        student_closest_gpa = temp
+
+            # print found students
+            if len(students_under_1) > 0:
+                print("Your student(s):")
+                print(f'{"ID":10}{"First Name":14}{"Last Name":14}{"GPA":7}')
+                print('-' * 45)
+                for student in students_under_1:
+                    print_student(student)
+                print()
+            if len(students_under_25) > 0:
+                print("You may also consider:")
+                print(f'{"ID":10}{"First Name":14}{"Last Name":14}{"GPA":7}')
+                print('-' * 45)
+                for student in students_under_25:
+                    print_student(student)
+                print()
+            if len(students_under_1) == 0 and len(students_under_25) == 0 and student_closest_gpa != None:
+                print("Your student with desired GPA was not found")
+                print("Student with nearest GPA:")
+                print(f'{"ID":10}{"First Name":14}{"Last Name":14}{"GPA":7}')
+                print('-' * 45)
+                print_student(student_closest_gpa)
+                print()
+            elif len(students_under_1) == 0 and len(students_under_25) == 0:
+                print("No such student")
+
+        user_input = input("Provide Student major and GPA (type \'q\' to quit): \n")
+        print()
+        user_input_lower = user_input.lower()
+
+if __name__ == "__main__":
+    students_majors_list = "FinalProjectStudentsMajorsList.csv"
+    gpa_list = "FinalProjectGPAList.csv"
+    graduation_dates_list = "FinalProjectGraduationDatesList.csv"
+    student_manager = Students(students_majors_list, gpa_list, graduation_dates_list)
+
+    print_menu(student_manager)
